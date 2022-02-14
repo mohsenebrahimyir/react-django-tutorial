bbcc93f34ce5ac5be23204c4b2d7faeef3438ce0
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-02-14 22:27:07 +0330

    add frontend app and install reactjs for frontend

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,6 @@
-from xml.etree.ElementInclude import include
 from django.urls import path
 from .views import RoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('room/', RoomView.as_view()),
 ]
--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     ## create apps    
     'api.apps.ApiConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'music_controller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
